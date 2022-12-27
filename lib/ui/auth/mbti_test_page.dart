8ae302bc584bd9a82523a4598f9c98de01bb179a
glow-kim <rkwhr1400@gmail.com>
2022-12-27 20:53:09 +0900

    [UPDATE] MBTI 테스트 UI 수정

--- lib/ui/auth/mbti_test_page.dart
+++ lib/ui/auth/mbti_test_page.dart
@@ -113,33 +113,39 @@ class _MBTITestScreenState extends State<MBTITestScreen> {
                 ),
               ),
               body: SafeArea(
-                child: Padding(
-                    padding: EdgeInsets.all(24.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (result == false)
-                          LinearPercentIndicator(
-                            width: phoneSize.width * 0.8,
-                            lineHeight: 14.0,
-                            percent:
-                                percent / 100, //firebase 에서 문제 번호 받아와서 바뀌게 해야함
-                            center: Text(
-                              percent.toString(),
-                              style: TextStyle(fontSize: 12.0),
-                            ),
-                            linearStrokeCap: LinearStrokeCap.roundAll,
-                            backgroundColor: Colors.white,
-                            progressColor: Colors.blue,
-                          ),
-                        if (result == false)
-                          _buildForm1(context, option1, option2),
-                        if (result == true) _buildForm2(context)
-                      ],
-                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    LinearPercentIndicator(
+                      width: phoneSize.width,
+                      padding: EdgeInsets.zero,
+                      lineHeight: 4.0,
+                      percent:
+                      percent / 100, //firebase 에서 문제 번호 받아와서 바뀌게 해야함
+                      center: Text(
+                        percent.toString(),
+                        style: TextStyle(fontSize: 12.0),
+                      ),
+                      linearStrokeCap: LinearStrokeCap.roundAll,
+                      backgroundColor: Color(0xffF5F5F5),
+                      progressColor: Color(0xff4975FF),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(24.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            if (result == false)
+                              _buildForm1(context, option1, option2),
+                            if (result == true) _buildForm2(context)
+                          ],
+                        )),
+                  ],
+                ),
               ),
               bottomNavigationBar: Material(
-                color: next == false ? Colors.white : Colors.blueAccent,
+                color: next == false ? Color(0xffD0D3E5) : Color(0xff4D64F3),
                 child: InkWell(
                   onTap: () {
                     setState(() {
@@ -166,8 +172,9 @@ class _MBTITestScreenState extends State<MBTITestScreen> {
                       child: Text(
                         index == 11 ? '결과 보기' : '다음',
                         style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                          fontWeight: FontWeight.w600,
                           color: Colors.white,
+                          fontSize: 14
                         ),
                       ),
                     ),
@@ -186,15 +193,19 @@ class _MBTITestScreenState extends State<MBTITestScreen> {
         child: SingleChildScrollView(
       // physics: NeverScrollableScrollPhysics(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
               '나는',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 24
+              ),
             ),
-            SizedBox(height: phoneSize.height * 0.01),
+            SizedBox(height: phoneSize.height * 0.05),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -213,38 +224,43 @@ class _MBTITestScreenState extends State<MBTITestScreen> {
                     });
                   },
                   child: Container(
+                    padding: EdgeInsets.only(left: 50, right: 50),
                       width: phoneSize.width * 0.8,
                       height: phoneSize.height * 0.15,
                       decoration: BoxDecoration(
-                          color: first == true
-                              ? Colors.blueAccent
-                              : Colors.white, //여기 색 바꾸면 됨
-                          borderRadius: BorderRadius.circular(12),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            )
-                          ]),
+                        gradient: first == true ? LinearGradient(
+                            colors: [Color(0xff3462FF), Color.fromRGBO(70, 111, 255, 0.6)],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter
+                        )
+                            : LinearGradient(colors: [Color.fromRGBO(255, 255, 255, 0.9), Color.fromRGBO(255, 255, 255, 0.9)]),
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 3,
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
+                          ),
+                        ],
+                      ),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(
-                            '첫번째 질문',
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
                           Text(
                             option1,
                             textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: first == true ? Colors.white : Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500
+                            ),
                           )
                         ],
                       )),
                 ),
-                SizedBox(height: phoneSize.height * 0.05),
+                SizedBox(height: phoneSize.height * 0.04),
                 InkWell(
                   onTap: () {
                     //선택 되면 색 바뀌고
@@ -259,33 +275,38 @@ class _MBTITestScreenState extends State<MBTITestScreen> {
                     });
                   },
                   child: Container(
+                      padding: EdgeInsets.only(left: 50, right: 50),
                       width: phoneSize.width * 0.8,
                       height: phoneSize.height * 0.15,
                       decoration: BoxDecoration(
-                          color: second == true
-                              ? Colors.blueAccent
-                              : Colors.white, //여기 색 바꾸면 됨
-                          borderRadius: BorderRadius.circular(12),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            )
-                          ]),
+                        gradient: second == true ? LinearGradient(
+                            colors: [Color(0xff3462FF), Color.fromRGBO(70, 111, 255, 0.6)],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter
+                        )
+                            : LinearGradient(colors: [Color.fromRGBO(255, 255, 255, 0.9), Color.fromRGBO(255, 255, 255, 0.9)]),
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.3),
+                            spreadRadius: 3,
+                            blurRadius: 10,
+                            offset: Offset(1, 1),
+                          ),
+                        ],
+                      ),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Text(
-                            '두번째 질문',
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
                           Text(
                             option2,
                             textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: second == true ? Colors.white : Colors.black,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500
+                            ),
                           )
                         ],
                       )),
@@ -299,16 +320,50 @@ class _MBTITestScreenState extends State<MBTITestScreen> {
   }
 
   Widget _buildForm2(BuildContext context) {
-    return Container(
+    var phoneSize = MediaQuery.of(context).size;
+    return SingleChildScrollView(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('안녕하세요 당신의 mbti는 $mbti 입니다'),
+          Placeholder(fallbackHeight: 150,),//MBTI 캐릭터 프로필 집어넣기
+          SizedBox(height: phoneSize.height*0.03,),
+          Text(
+            '$mbti',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w700
+            ),
+          ),
+          Text(
+            'MBTI 이름(재기발랄 활동가 이런거 가져오기)',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 16
+            ),
+          ),
+          SizedBox(height: phoneSize.height*0.03),
+          Text(
+            '닉네임님의 MBTI 는 $mbti입니다.\n'
+                '더 자세한 검사는 정식테스트를 진행해주세요.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(height: phoneSize.height*0.03),
           Container(
             child: InkWell(
               onTap: (){
                 Navigator.pop(context);
               },
-              child: Text('돌아가기'),
+              child: Text(
+                  '이전 페이지로 돌아가기',
+                style: TextStyle(
+                  color: Color(0xff466FFF)
+                ),
+              ),
             ),
           )
         ],
