45d8ddf818c2f3879706bf1365a848b723f450c1
glow-kim <rkwhr1400@gmail.com>
2022-12-29 16:22:28 +0900

    [UPDATE] 내정보 보기 page ui 수정 완료

--- lib/ui/setting/my_information_page.dart
+++ lib/ui/setting/my_information_page.dart
@@ -68,6 +68,8 @@ class _MyPageScreenState extends State<MyPageScreen> {
           '내 정보',
           style: TextStyle(
             color: Colors.black,
+            fontWeight: FontWeight.w700,
+            fontSize: 16
           ),
         ),
         leading: IconButton(
@@ -87,7 +89,12 @@ class _MyPageScreenState extends State<MyPageScreen> {
                   MaterialPageRoute(builder: (context) => MyPageEditScreen()));
             },
             child: Text(
-                '수정하기'
+                '수정하기',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Color(0xff4975FF)
+              ),
             ),
 
           )
@@ -119,25 +126,43 @@ class _MyPageScreenState extends State<MyPageScreen> {
                 padding: const EdgeInsets.symmetric(horizontal: 24),
                 child: Column(
                   children: <Widget>[
-                    Placeholder(fallbackHeight: 120,fallbackWidth: 120), // 프로필 불러오기
-                    SizedBox(height: phoneSize.height * 0.01),
+                    Placeholder(fallbackHeight: 120,fallbackWidth: 120), // 프로필 불러오기 해야함!!
+                    SizedBox(height: phoneSize.height * 0.04),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           '닉네임',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 14
+                          ),
                         ),
+                        SizedBox(height: phoneSize.height * 0.01),
                         Container(
                           height: phoneSize.height * 0.08,
-                          padding: EdgeInsets.only(left:10.0, right:10.0),
+                          padding: EdgeInsets.only(left:18.0, right:10.0),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(12.0),
                             color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(110, 113, 145, 0.12).withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: Offset(0, 3),
+                              )
+                            ],
                           ),
                           child: Row(
                             children: [
                               Text(
                                 userData.name,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xffA0A3BD)
+                                ),
                               ),
 
                             ],
@@ -153,18 +178,36 @@ class _MyPageScreenState extends State<MyPageScreen> {
                       children: [
                         Text(
                           'MBTI',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14
+                          ),
                         ),
+                        SizedBox(height: phoneSize.height * 0.01),
                         Container(
                           height: phoneSize.height * 0.08,
-                          padding: EdgeInsets.only(left:10.0, right:10.0),
+                          padding: EdgeInsets.only(left:18.0, right:10.0),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(12.0),
                             color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(110, 113, 145, 0.12).withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: Offset(0, 3),
+                              )
+                            ],
                           ),
                           child: Row(
                             children: [
                               Text(
                                 userData.mbti,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  color: Color(0xffA0A3BD)
+                                ),
                               ),
 
                             ],
@@ -180,18 +223,36 @@ class _MyPageScreenState extends State<MyPageScreen> {
                       children: [
                         Text(
                           '이메일',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14
+                          ),
                         ),
+                        SizedBox(height: phoneSize.height * 0.01),
                         Container(
                           height: phoneSize.height * 0.08,
                           padding: EdgeInsets.only(left:10.0, right:10.0),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(12.0),
                             color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(110, 113, 145, 0.12).withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: Offset(0, 3),
+                              )
+                            ],
                           ),
                           child: Row(
                             children: [
                               Text(
-                                userData.email
+                                userData.email,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                    color: Color(0xffA0A3BD)
+                                ),
                               ),
                             ],
                           ),
@@ -206,18 +267,36 @@ class _MyPageScreenState extends State<MyPageScreen> {
                       children: [
                         Text(
                           '비밀번호',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14
+                          ),
                         ),
+                        SizedBox(height: phoneSize.height * 0.01),
                         Container(
                           height: phoneSize.height * 0.08,
                           padding: EdgeInsets.only(left:10.0, right:10.0),
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(12.0),
                             color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(110, 113, 145, 0.12).withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: Offset(0, 3),
+                              )
+                            ],
                           ),
                           child: Row(
                             children: [
                               Text(
-                                '⚫⚫⚫⚫⚫⚫⚫'
+                                '⚫⚫⚫⚫⚫⚫⚫',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 7,
+                                    color: Color(0xffA0A3BD) //얘는 색깔이 안 바뀜 ㅋㅋㅋㅋㅋㅋㅋㅋ
+                                ),
                               )
                             ],
                           ),
@@ -236,24 +315,42 @@ class _MyPageScreenState extends State<MyPageScreen> {
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(12.0),
                             color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(110, 113, 145, 0.12).withOpacity(0.1),
+                                spreadRadius: 1,
+                                blurRadius: 8,
+                                offset: Offset(0, 3),
+                              )
+                            ],
                           ),
                           child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Container(
-                                padding: EdgeInsets.only(right : 16.0),
-                                child: Icon(
-                                  Icons.shield,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              Text(
-                                '정보 동의 설정',
+                              Row(
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.only(right : 16.0),
+                                    child: Icon(
+                                      Icons.shield,
+                                      color: Color(0xffA0A3BD),
+                                    ),
+                                  ),
+                                  Text(
+                                    '정보 동의 설정',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xffA0A3BD)
+                                    ),
+                                  ),
+                                ],
                               ),
                               Switch(
                                 value: isSwitched,
                                 onChanged: (value) {
                                   setState(() {
-                                    isSwitched = false; //firebase 에서 값 가져와서 고정하기! 지금은 임의로 무조건 false 로 해놨음
+                                    isSwitched = false; //firebase 에서 동의 했는지 값 가져와서 고정하기! 지금은 임의로 무조건 false 로 해놨음
                                   });
                                 },
                                 activeTrackColor: Colors.blue,
@@ -281,6 +378,14 @@ class _MyPageScreenState extends State<MyPageScreen> {
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(12.0),
                               color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Color.fromRGBO(110, 113, 145, 0.12).withOpacity(0.1),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                  offset: Offset(0, 3),
+                                )
+                              ],
                             ),
                             child: Row(
                               children: [
@@ -288,11 +393,16 @@ class _MyPageScreenState extends State<MyPageScreen> {
                                   padding: EdgeInsets.only(right : 16.0),
                                   child: Icon(
                                     Icons.mood_bad,
-                                    color: Colors.grey,
+                                    color: Color(0xffA0A3BD),
                                   ),
                                 ),
                                 Text(
                                   '탈퇴하기',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 14,
+                                    color: Color(0xffA0A3BD)
+                                  ),
                                 ),
                               ],
                             ),
